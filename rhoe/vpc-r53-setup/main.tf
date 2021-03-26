cc63dc8acc2cf90364de7eaad90acf0865ea5d3b
Ken Moini <ken@kenmoini.com>
2021-03-26 16:18:33 -0400

    Update main.tf

--- rhoe/vpc-r53-setup/main.tf
+++ rhoe/vpc-r53-setup/main.tf
@@ -1,9 +1,3 @@
-resource "aws_vpc" "primary" {
-  cidr_block           = "10.42.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-}
-
 resource "aws_route53_zone" "r53domain" {
   name = "EXAMPLE.COM"
 
